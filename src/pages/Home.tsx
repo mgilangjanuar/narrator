2848558a4a818633eec20b7a7ed94f6befeb8543
M Gilang Januar <mgilangjanuar@gmail.com>
2022-09-16 13:20:37 +0700

    update /go page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,14 +10,14 @@ function Home() {
         Build a bullsh💩t story that is easy to go viral in a minute.
       </Typography.Title>
       <br />
-      <TwitterTweetEmbed tweetId="1562711157885939712" />
-      <TwitterTweetEmbed tweetId="1562750163273515011" />
-      <br /><br />
       <Typography.Paragraph style={{ textAlign: 'center' }}>
         <Button shape="round" size="large" type="primary" onClick={() => navigate('/go')}>
           Generate Now!
         </Button>
       </Typography.Paragraph>
+      <br /><br />
+      <TwitterTweetEmbed tweetId="1562711157885939712" />
+      <TwitterTweetEmbed tweetId="1562750163273515011" />
     </Layout.Content>
   )
 }
